ce00619d519a42194cc03364d955abb98f6e75d5
UK08PCB <umang.2023ug2004@iiitranchi.ac.in>
2024-09-23 21:25:57 +0530

    Update lab1.2.1.py

--- lab1.2.1.py
+++ lab1.2.1.py
@@ -4,4 +4,5 @@ b=int(input("enter the side side2:"))
 c=int(input("enter the side side3:"))
 s=(a+b+c)/2
 area=m.sqrt(s*(s-a)*(s-b)*(s-c))
-print(area)
+print("the sides of triangle are:",a,b,c)
+print("the area of the triangle is:",area)
