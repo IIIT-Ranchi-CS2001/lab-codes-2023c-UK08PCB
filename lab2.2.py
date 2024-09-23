33272e42de1e61ac228ce0d4e5c63c46a0ddc1c9
UK08PCB <umang.2023ug2004@iiitranchi.ac.in>
2024-09-23 21:50:22 +0530

    Add files via upload

---
+++ lab2.2.py
@@ -0,0 +1,5 @@
+s="Ba Ba Black Sheep"
+print("length of the string is",len(s))
+print("the first occurance of letter \"e\" is:",s.find('e'))
+print("the total number of occurance of \"a\" is:",s.count('a'))
+print(s.replace('B','T',2))
